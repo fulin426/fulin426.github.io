c810305e5fdc19b92d7f658fad0f944a97e701cf
Fu-Lin Liu <fulin426@gmail.com>
2018-12-23 08:45:28 -0800

    set up colors and nav bar

---
+++ client.js
@@ -0,0 +1,22 @@
+$('.profile-about').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	console.log(jump);
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 10 }, 500);
+});
+
+$('.projects-nav').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 10 }, 500);
+});
+
+$('.contact').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 10 }, 500);
+});
+
